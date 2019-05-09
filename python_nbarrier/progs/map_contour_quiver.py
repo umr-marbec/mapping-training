e64ed33d23200224d4168ac62aa70dd66314bc43
barriern <nicolas.barrier@ird.fr>
2019-05-09 15:19:04 +0200

    Rename files + some changes

--- python_nbarrier/progs/map_contour_quiver.py
+++ python_nbarrier/progs/map_contour_quiver.py
@@ -6,18 +6,19 @@ m = Basemap(llcrnrlon=np.min(lon), llcrnrlat=np.min(lat),
             urcrnrlon=np.max(lon), urcrnrlat=np.max(lat), 
             projection='cyl', resolution='c')
 
-# conversion from geo. to map coordinate
+# conversion from geo. to map coordinate 
 lon2d, lat2d = np.meshgrid(lon, lat) # needs 2D coord. arrays
 x, y = m(lon2d, lat2d)
 
-# map background
+# map background using basemap function
 m.fillcontinents(color='gray', lake_color='gray')  
 
-# drawing contour lines
+# drawing contour lines using Basemap function
 cs = m.contour(x, y, z, colors='k', linewidths=0.5)
 plt.clabel(cs, fmt='%.2f')
 
 # drawing quiver plot
+# use of pyplot function !!!
 q = plt.quiver(x, y, u, v, z, cmap=plt.cm.get_cmap('hsv'), scale=1000, zorder=1000)
 q.set_clim(0, 50)
 cb = m.colorbar(q)
